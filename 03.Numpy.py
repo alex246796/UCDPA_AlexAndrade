dd82b447775b6b544579895d4a56571bc8f4a2fa
alex246796 <alex246796@gmail.com>
2021-09-23 23:28:18 +0100

    The end

--- 03.Numpy.py
+++ 03.Numpy.py
@@ -11,3 +11,9 @@ print(Years_array1)
 indexing_array = np.array([0,5])
 Years_array2 = Years_array1[indexing_array]
 print(Years_array2)
+
+
+
+# Define a custom function to create reusable code
+
+
