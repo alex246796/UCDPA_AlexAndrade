2446c7437bec0bafc33b42dbbaa6cfa94a81f13d
alex246796 <alex246796@gmail.com>
2021-09-23 16:09:23 +0100

    Indexing, Grouping, Sorting

--- 02.API.py
+++ 02.API.py
@@ -1,3 +1,5 @@
+# importing API
+
 import requests
 AverageWeeklyRent = requests.get('https://ws.cso.ie/public/api.jsonrpc?data=%7B%22jsonrpc%22:%222.0%22,%22method%22:%22PxStat.Data.Cube_API.ReadDataset%22,%22params%22:%7B%22class%22:%22query%22,%22id%22:%5B%22STATISTIC%22%5D,%22dimension%22:%7B%22STATISTIC%22:%7B%22category%22:%7B%22index%22:%5B%22E1021C13%22%5D%7D%7D%7D,%22extension%22:%7B%22pivot%22:null,%22codes%22:false,%22language%22:%7B%22code%22:%22en%22%7D,%22format%22:%7B%22type%22:%22JSON-stat%22,%22version%22:%222.0%22%7D,%22matrix%22:%22E1021%22%7D,%22version%22:%222.0%22%7D%7D')
 
@@ -15,3 +17,5 @@ AverageWeeklyRent3 = pd.DataFrame (AverageWeeklyRent2)
 
 print(AverageWeeklyRent3)
 
+# My initial goal was to use this dataset to compare rents vs house sales,
+# However, I did not manage to extract the pieces I needed to build any insights
--- 03.Numpy.py
+++ 03.Numpy.py
@@ -1,10 +1,13 @@
+# Creating arrays with numpy of the years that Will be worked in the project
+
 import numpy as np
 
 Years_array1 = np.array(['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'])
+print(Years_array1)
+
+
+
+# Creating arrays with numpy of the years that Will be worked in the comparisons with rent market through the API
 indexing_array = np.array([0,5])
 Years_array2 = Years_array1[indexing_array]
-
-
-
-print(Years_array1)
 print(Years_array2)
